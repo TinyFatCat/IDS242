a66d2567f9d1d5595498ba1722ae51740bdd3b3c
TinyFatCat <126849634+TinyFatCat@users.noreply.github.com>
2024-11-30 17:27:51 +0900

    url fix

--- src/component/Header.js
+++ src/component/Header.js
@@ -6,16 +6,22 @@ const Header = () => {
   const pageRefresh = () => {
     window.location.reload();
   };
+  const icon__dic = process.env.PUBLIC_URL + '/img/ui/header_dictionary.webp';
+  const icon__home = process.env.PUBLIC_URL + '/img/ui/header_home.webp';
   return (
     <div className={styles.header}>
       <div className={styles.header__wrapper}>
         <div className={styles.header__icon}>
-          <div className={styles.header__icon__dic} onClick={pageRefresh}></div>
+          <div className={styles.header__icon__dic} onClick={pageRefresh}>
+            <img src={icon__dic}></img>
+          </div>
           <h3>새로고침</h3>
         </div>
         <div className={styles.header__icon}>
           <Link to="/">
-            <div className={styles.header__icon__home}></div>
+            <div className={styles.header__icon__home}>
+              <img src={icon__home}></img>
+            </div>
             <h3>타이틀로</h3>
           </Link>
         </div>
